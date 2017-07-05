dec78ef66db2c772b5c93d8a9d5e1088b0c92f11
chio9.chio9 <chio-nzgft@users.noreply.github.com>
2017-07-05 11:11:34 +0800

    Update OCS-db-print.go

--- OCS-db-print.go
+++ OCS-db-print.go
@@ -6,7 +6,7 @@ import (
     "fmt"
     "os"
 )
-
+//set db info
 const (
     DB_HOST = "tcp(127.0.0.1:3306)"
     DB_NAME = "ocsdb"
@@ -14,49 +14,10 @@ const (
     DB_PASS = "ocspass"
 )
 
-func checkCount(rows *sql.Rows) (count int) {
-        for rows.Next() {
-        err:= rows.Scan(&count)
-        checkErr("Fail to get count ",err )
-    }
-    return count
-}
-
-func checkRow(rows *sql.Rows)(int) {
-        cols, err:=  rows.Columns()
-        checkErr("Failed to get columns",err )
-        return len(cols)
-}
-
-
-func checkErr(w_msg string, err error) {
-    if err != nil {
-       fmt.Println(w_msg,err)
-     os.Exit(2)
-     return
-    }
-}
-
-func main() {
-    var len_count,len_cols int
-    dsn := DB_USER + ":" + DB_PASS + "@" + DB_HOST + "/" + DB_NAME + "?charset=utf8"
-    db, err := sql.Open("mysql", dsn)
-    checkErr("Failed to run query",err)
-    defer db.Close()
-
-    rows_count, err := db.Query("select COUNT(*) as count from accountinfo")
-    checkErr("Failed to run query for count", err)
-    len_count = checkCount(rows_count)
-    defer rows_count.Close()
-
-    rows, err := db.Query("select * from accountinfo")
-    checkErr("Failed to run query", err)
-    len_cols = checkRow(rows)
-    defer rows.Close()
-
+// DB to 2D string
+func DBtoString(rows *sql.Rows,len_cols int, len_count int)([][]string) {
     rawResult := make([][]byte, len_cols)
     result := make([][]string,  len_cols)
-
      for i := range result {
         result[i] = make([]string, len_count)
     }
@@ -69,7 +30,7 @@ func main() {
     k = -1
     for rows.Next() {
         k = k +1
-        err = rows.Scan(dest...)
+        err := rows.Scan(dest...)
         checkErr("Failed to scan row", err)
 
         for i, raw := range rawResult {
@@ -79,11 +40,71 @@ func main() {
                 result[i][k] = string(raw)
             }
         }
-
-        for i:= 0; i < len_cols ; i++ {
-               fmt.Print(result[i][k])
-               fmt.Print(" ")
-        }
-        fmt.Println(" ")
     }
+ return result
+}
+
+//Get select count line
+func checkCount(rows *sql.Rows) (count int) {
+        for rows.Next() {
+        err:= rows.Scan(&count)
+        checkErr("Fail to get count ",err )
+    }
+    return count
+}
+
+//Get select table rows
+func checkRow(rows *sql.Rows)(int) {
+        cols, err:=  rows.Columns()
+        checkErr("Failed to get columns",err )
+        return len(cols)
+}
+
+//Show error & exit
+func checkErr(w_msg string, err error) {
+    if err != nil {
+       fmt.Println(w_msg,err)
+     os.Exit(2)
+     return
+    }
+}
+
+func main() {
+    var len_count,len_cols int
+    
+    // connect db
+    dsn := DB_USER + ":" + DB_PASS + "@" + DB_HOST + "/" + DB_NAME + "?charset=utf8"
+    db, err := sql.Open("mysql", dsn)
+    checkErr("Failed to run query",err)
+    defer db.Close()
+
+    //query get count 
+    rows_count, err := db.Query("select COUNT(*) as count from accountinfo")
+    checkErr("Failed to run query for count", err)
+    len_count = checkCount(rows_count)
+    defer rows_count.Close()
+
+    //query get rows
+    rows, err := db.Query("select * from accountinfo")
+    checkErr("Failed to run query", err)
+    len_cols = checkRow(rows)
+    defer rows.Close()
+
+    //set up 2D string by len_cols & len_count
+    result := make([][]string,  len_cols)
+     for i := range result {
+        result[i] = make([]string, len_count)
+     }
+
+    //Use DB to get 2D String
+    result =  DBtoString(rows,len_cols,len_count)
+
+    //juest print 2D String 
+    for i:=0 ; i < len_count ; i++{
+      for j:=0 ; j < len_cols ; j++{
+       fmt.Print(result[j][i])
+       fmt.Print(" ")
+      }
+      fmt.Println("")
+   }
 }
